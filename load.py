bc36d690b69dda069cf8e6cd86223bf14d33eee8
amen <zju3020912063@gmail.com>
2012-08-03 19:32:31 +0800

    Signed-off-by: amen <zju3020912063@gmail.com>

--- load.py
+++ load.py
@@ -2,46 +2,47 @@
 import re
 import copy
 import ujson
-fp=open('chinese_data.txt','r')
-all_line=fp.readlines()
-fp.close()
 
 class WordCell(dict):
     word_ref=None
     word_pre=None
+    def BuildFindTree(self,all_line):
+        for line in all_line:
+            line=line.strip()
+            line_text=line.decode('utf-8')
 
-def BuildFindTree(all_line):
-    word_dict_root=WordCell()
-    for line in all_line:
-        line=line.strip()
-        line_text=line.decode('utf-8')
-
-        startcell=None
-        if word_dict_root.has_key(line_text[0]):
-            startcell=word_dict_root[line_text[0]]
-        else:
-            startcell=WordCell()
-            word_dict_root[line_text[0]]=startcell
-        for word in line_text[1:]:
-            thiscell=None
-            if startcell.has_key(word):
-                thiscell=startcell[word]
+            startcell=None
+            if self.has_key(line_text[0]):
+                startcell=self[line_text[0]]
             else:
-                thiscell=WordCell()
-                thiscell.word_pre=startcell
-                startcell[word]=thiscell
-            startcell=thiscell
-        startcell.word_ref=line_text
-    return word_dict_root
+                startcell=WordCell()
+                self[line_text[0]]=startcell
+            for word in line_text[1:]:
+                thiscell=None
+                if startcell.has_key(word):
+                    thiscell=startcell[word]
+                else:
+                    thiscell=WordCell()
+                    thiscell.word_pre=startcell
+                    startcell[word]=thiscell
+                startcell=thiscell
+            startcell.word_ref=line_text
 
-word_dict_root=BuildFindTree(all_line)
+word_dict_root=WordCell()
+
+fp=open('chinese_data.txt','r')
+all_line=fp.readlines()
+fp.close()
+word_dict_root.BuildFindTree(all_line)
 
 class FoundWord:
     word=None
     pos=-1
-    def __init__(self,str,nowpos):
+    tree_pos=None
+    def __init__(self,str,nowpos,treepos):
         self.word=str
         self.pos=nowpos-len(str)
+        self.tree_pos=treepos
 
 def ProcessLine(word_dict_root,line):
     process_work=[]
@@ -53,14 +54,18 @@ def ProcessLine(word_dict_root,line):
     number=''
     for index in range(len(line)):
         char=line[index]
-        if char in number_set:
+        if char in number_set:#检查数词的存在
+            for one_proc in process_work:
+                if one_proc.word_ref!=None:
+                    found_word.append(FoundWord(one_proc.word_ref,index,one_proc))
+            process_work=[]
             number=number+char
         else:
             read_number=number
             number=''
 
             if len(read_number)>0:
-                found_word.append(FoundWord(read_number,index))
+                found_word.append(FoundWord(read_number,index,None))
                 process_work.append(word_dict_root['n'])
 
         if len(process_work)==0:
@@ -69,7 +74,7 @@ def ProcessLine(word_dict_root,line):
         need_create_new_process=False
         has_one_success=False
         for one_proc in process_work:
-            if one_proc.has_key(char):
+            if one_proc.has_key(char): #检查下一个字
                 has_one_success=True
                 next=one_proc[char]
                 next_round_process_word.append(next)
@@ -78,14 +83,14 @@ def ProcessLine(word_dict_root,line):
                     need_create_new_process=True
             else:
                 if one_proc.word_ref!=None:
-                    found_word.append(FoundWord(one_proc.word_ref,index))
-                else:
+                    found_word.append(FoundWord(one_proc.word_ref,index,one_proc))
+                else: #无法找到词语的时候，回溯比当前位置短的最长词语
                     pre_index=0
                     while one_proc.word_pre!=None:
                         pre_index+=1
                         one_proc=one_proc.word_pre
                         if one_proc.word_ref!=None:
-                            found_word.append(FoundWord(one_proc.word_ref,index-pre_index))
+                            found_word.append(FoundWord(one_proc.word_ref,index-pre_index,one_proc))
                             break
 
         if has_one_success==False:
@@ -97,31 +102,86 @@ def ProcessLine(word_dict_root,line):
 
         process_work=next_round_process_word
 
+    if len(number)>0:
+        found_word.append(FoundWord(number,len(line),None))
     for one_proc in process_work:
         if one_proc.word_ref!=None:
-            found_word.append(FoundWord(one_proc.word_ref,len(line)))
+            found_word.append(FoundWord(one_proc.word_ref,len(line),one_proc))
 
-    if len(found_word)>=2:
+    while True:
+        gorecheck=False
+        if len(found_word)>=3: #检查当前词语刚好前半部分是前一个词后半部分是后一个词 当前词删除
+            for index in range(len(found_word)-2,0,-1):
+                pre_word=found_word[index-1]
+                #if len(pre_word.word)==1:
+                #    continue
+                aft_word=found_word[index+1]
+                if len(aft_word.word)==1:
+                    continue
+                now_word=found_word[index]
+
+                for i2 in range(1,len(now_word.word)):
+                    if pre_word.word.endswith(now_word.word[0:i2]):
+                        if aft_word.word.startswith(now_word.word[i2:]):
+                            gorecheck=True
+                            del found_word[index]
+                            break
+                if gorecheck:
+                    break
+        if gorecheck==False:
+            break
+
+    if len(found_word)>=2: #检查前一个词语是当前词语的一部分 全文索引不需要这个
+        res_found_word=[]
+        first_word=found_word[0]
+        for index in range(1,len(found_word)):
+            word=found_word[index]
+            if word.word.find(first_word.word)==-1:
+                res_found_word.append(first_word)
+            first_word=word
+        res_found_word.append(found_word[len(found_word)-1])
+        found_word=res_found_word
+
+    #后词包含前词的结尾的时候，重叠部分归后词
+    for index in range(len(found_word)-1,0,-1):
+        aft_word=found_word[index]
+        now_word=found_word[index-1]
+        while True:
+            recheck=False
+            for i2 in range(1,len(aft_word.word)):
+                word_pice=aft_word.word[0:i2]
+                if now_word.word.endswith(word_pice):
+                    if now_word.tree_pos!=None:
+                        pre_pos=now_word.tree_pos.word_pre
+                        while pre_pos!=None and pre_pos.word_ref==None:
+                            pre_pos=pre_pos.word_pre
+                        if pre_pos!=None:
+                            now_word.word=pre_pos.word_ref
+                            now_word.tree_pos=pre_pos
+                            recheck=True
+                            break
+                    else:
+                        break
+            if recheck==False:
+                break
+
+    if len(found_word)>=2: #检查下一个词语是当前词语的后半部分 全文索引不需要这个
         res_found_word=[]
         last_word=found_word[len(found_word)-1]
         for index in range(len(found_word)-2,-1,-1):
             word=found_word[index]
-            if word.word.endswith(last_word.word):
-                last_word=word
-            else:
+            if word.word.endswith(last_word.word)==False:
                 res_found_word.append(last_word)
-                last_word=word
+            last_word=word
         res_found_word.append(found_word[0])
         res_found_word.reverse()
         found_word=res_found_word
+
     return found_word
 
-full_text=u"""除苏拉以外，今年第10号台风“达维”也已在西北太平洋上生成，目前正向中国沿海逼近。预计将于2日傍晚到3日早晨在江苏启东到山东青岛沿海一带登陆。
-据介绍，此次台风有六个特点：一是双台风活动，路径变数大。二是风圈半径较大，中心风力强。三是鼎盛期正面登陆，防御战线长。四是可能深入内陆，影响范围广。五是多地引发强降雨，持续时间长。六是风雨潮遭遇，防御难度大.
-中央气象台1日18时发布暴雨蓝色预警，预计1日20时至2日20时，浙江东部、福建东部、台湾等地有大到暴雨，其中，浙江南部沿海和福建北部沿海局地有大暴雨(100～140毫米)，台湾中北部有大暴雨，部分地区有特大暴雨(300～500毫米)。
-国家防总透露，从目前预测情况看，“苏拉”、“达维”登陆后可能深入内陆影响长江流域的江西以及淮河、黄河甚至海河流域的江苏、安徽、山东、河南以及华北等地。预计两个台风的影响范围将跨长江中下游、淮河、黄河中下游、海河等流域10多个省份。
-由于南北方前期降雨过程多、洪水量级大、土壤含水量趋于饱和，遇强降雨极易引发大的洪涝灾害，局部地区可能造成严重的山洪、泥石流和滑坡等次生灾害。""";
-
+fp=open('testdata.txt','r')
+full_text=fp.read().decode('utf-8')
+fp.close()
 text_pice=re.split(u"[\s!?,。；，：“ ”（ ）、？《》·]",full_text)
 text_list=[]
 for tp in text_pice:
