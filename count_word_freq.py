268f2a3a931e815356dffa5cb092004e85245875
amen <zju3020912063@gmail.com>
2012-08-27 19:43:51 +0800

    Signed-off-by: amen <zju3020912063@gmail.com>

---
+++ count_word_freq.py
@@ -0,0 +1,19 @@
+#-*-coding:utf-8-*-
+from weibo_autooauth import *
+import sqlite3
+import time
+import codecs
+import ujson as json
+
+if __name__ == '__main__':
+    dbtext=sqlite3.connect("data/fulltext.db")
+
+    dc=dbtext.cursor()
+    dc.execute('select word,sum(times) from all_word group by word')
+
+    fp=codecs.open('data/word_freq.txt','w+','utf-8')
+    word_dic={}
+    for line in dc:
+        word_dic[line[0]]=line[1]
+    json.dump(word_dic,fp)
+    fp.close()
