fedc8707e3ece69091dee0f198352b55b6747512
amen <amen@localhost.localdomain>
2013-01-24 22:38:17 -0500

    Signed-off-by: amen <amen@localhost.localdomain>

--- FindResponse.py
+++ FindResponse.py
@@ -10,19 +10,10 @@ import weibo_bot
 import sqlite3
 import math
 
-import Tkinter
-
 #用词语权重法收集接近的句子
 def Step1(word_dict_root,word_record):
     db=sqlite3.connect("data/dbforsearch.db")
     dbc=db.cursor()
-
-    main_weight=0
-    for key in word_record:
-        word_info=word_dict_root.getwordinfo(key)
-        if word_info!=None:
-            if 'weight' in word_info:
-                main_weight+=1e4/word_info['weight']
 
     weibo_id_count={}
     for key in word_record:
@@ -32,20 +23,18 @@ def Step1(word_dict_root,word_record):
                 d_weight=1e4/word_info['weight']
 
                 dbc.execute("select weibo_id,times from all_word where word=?",(key,))
-                for resline in dbc:
-                    if resline[0] in weibo_id_count:
-                        weibo_id_count[resline[0]]+=resline[1]*d_weight;
-                    else:
-                        weibo_id_count[resline[0]]=resline[1]*d_weight;
+                for weibo_id,times in dbc:
+                    weibo_id_count[weibo_id]=weibo_id_count.get(weibo_id,0)+min(times,3)*d_weight;
 
-    w_id_count=[]
-    for id in weibo_id_count:
-        w_id_count.append((id,weibo_id_count[id]))
-    w_id_count.sort(lambda a,b:cmp(abs(a[1]-main_weight),abs(b[1]-main_weight)))
+    for weibo_id in weibo_id_count.keys():
+        dbc.execute("select word_count from all_weibo where weibo_id=?",(weibo_id,))
+        word_count,=dbc.next()
+        if float(abs(word_count-len(word_record)))/len(word_record)>0.2:
+            weibo_id_count.pop(weibo_id)
 
     word2_list={}
-    for i in xrange(0,min(800,len(w_id_count))):
-        dbc.execute("select weibo_id,word,times from all_word where weibo_id=?",(w_id_count[i][0],))
+    for id in weibo_id_count:
+        dbc.execute("select weibo_id,word,times from all_word where weibo_id=?",(id,))
         for weibo_id,word,time in dbc:
             if word in word2_list:
                 w2_l=word2_list[word]
@@ -60,60 +49,69 @@ def FindResponse(word_dict_root,word):
     word=weibo_bot.RemoveWeiboRubbish(word)
     word_record=weibo_bot.FindWordCount(word_dict_root,word)
     word2_list=Step1(word_dict_root,word_record)
-
     word_index=word2_list.keys()
     weibo_index=set()
     for key in word2_list:
         l2=word2_list[key]
         weibo_index.update(l2.keys())
-    weibo_index=list(weibo_index)
-    weibo_id_index=dict()
-    for i in xrange(0,len(weibo_index)):
-        weibo_id_index[weibo_index[i]]=i
+    if len(weibo_index)>2 and len(word_index)>2:
+        weibo_index=list(weibo_index)
+        weibo_id_index=dict()
+        for i in xrange(0,len(weibo_index)):
+            weibo_id_index[weibo_index[i]]=i
 
-    A=numpy.zeros((len(word_index),len(weibo_index)+1))
+        A=numpy.zeros((len(word_index),len(weibo_index)+1))
 
-    for i in xrange(0,len(word_index)):
-        word=word_index[i]
-        word_info=word_dict_root.getwordinfo(word)
-        if word_info!=None:
-            if 'weight' in word_info:
-                d_weight=1e4/word_info['weight']
-                ls=word2_list[word]
-                for wb_i in ls:
-                    j=weibo_id_index[wb_i]
-                    A[i,j]=ls[wb_i]*d_weight
-    for word in word_record:
-        if word in word_index:
+        for i in xrange(0,len(word_index)):
+            word=word_index[i]
             word_info=word_dict_root.getwordinfo(word)
             if word_info!=None:
                 if 'weight' in word_info:
                     d_weight=1e4/word_info['weight']
-                    i=word_index.index(word)
-                    A[i,len(weibo_index)]=word_record[word]*d_weight
+                    ls=word2_list[word]
+                    for wb_i in ls:
+                        j=weibo_id_index[wb_i]
+                        A[i,j]=ls[wb_i]*d_weight
+        for word in word_record:
+            if word in word_index:
+                word_info=word_dict_root.getwordinfo(word)
+                if word_info!=None:
+                    if 'weight' in word_info:
+                        d_weight=1e4/word_info['weight']
+                        i=word_index.index(word)
+                        A[i,len(weibo_index)]=word_record[word]*d_weight
 
-    A=scipy.matrix(A,copy=False)
-    L,a,R=scipy.linalg.svd(A,0)
+        A=scipy.matrix(A,copy=False)
+        L,a,R=scipy.linalg.svd(A,0)
 
-    weibo_pos=[]
-    weibo_x=R[1]
-    weibo_y=R[2]
-    for i in xrange(0,len(weibo_index)):
-        weibo_pos.append((weibo_x[i],weibo_y[i]))
+        weibo_pos=[]
+        weibo_x=R[1]
+        weibo_y=R[2]
 
-    weibo_pos_last=(R[1][len(weibo_index)],R[2][len(weibo_index)])
-    weibo_dis=[]
-    for i in xrange(0,len(weibo_pos)):
-        weibo_dis.append((weibo_index[i],math.sqrt( (weibo_pos_last[0]-weibo_pos[i][0])**2+ (weibo_pos_last[1]-weibo_pos[i][1])**2 )))
-    weibo_dis.sort(lambda a,b:cmp(a[1],b[1]))
+        weibo_pos_last=(weibo_x[len(weibo_index)],weibo_y[len(weibo_index)])
+        for i in xrange(0,len(weibo_index)):
+            weibo_pos.append((weibo_x[i],weibo_y[i]))
 
-    db=sqlite3.connect("data/dbforsearch.db")
-    dbc=db.cursor()
-    res_wordlist=[]
-    for one in weibo_dis[0:10]:
-        dbc.execute("select word from all_weibo where weibo_id=?",(one[0],))
-        word,=dbc.next()
-        res_wordlist.append(word)
+        weibo_dis=[]
+        for i in xrange(0,len(weibo_pos)):
+            weibo_dis.append((weibo_index[i],math.sqrt( (weibo_pos_last[0]-weibo_pos[i][0])**2+ (weibo_pos_last[1]-weibo_pos[i][1])**2 )))
+        weibo_dis.sort(lambda a,b:cmp(a[1],b[1]))
+
+        db=sqlite3.connect("data/dbforsearch.db")
+        dbc=db.cursor()
+        res_wordlist=[]
+        for one in weibo_dis[0:10]:
+            dbc.execute("select word from all_weibo where weibo_id=?",(one[0],))
+            word,=dbc.next()
+            res_wordlist.append(word)
+    else:
+        db=sqlite3.connect("data/dbforsearch.db")
+        dbc=db.cursor()
+        res_wordlist=[]
+        for one in weibo_index:
+            dbc.execute("select word from all_weibo where weibo_id=?",(one,))
+            word,=dbc.next()
+            res_wordlist.append(word)
     return res_wordlist
 if __name__ == '__main__':
     word_dict_root=decoder.LoadDefaultWordDic()
--- bot_http_server.py
+++ bot_http_server.py
@@ -1,10 +1,10 @@
 #-*-coding:utf-8-*-
 import web
-import time
+import string
 import json
 import weibo_bot
 import decoder
-import sqlite3
+import FindResponse
 
 urls = (
     '/bot', 'Bot',
@@ -18,11 +18,20 @@ class Bot:
         req=inputs.get('req')
         if isinstance(req,unicode)==False:
             return json.dumps({'error':1,'message':'request error'})
-        searchdb=sqlite3.connect("data/dbforsearch.db")
-        res=weibo_bot.FindReplyForSentence(word_dict_root,searchdb,req)
+        ver=int(inputs.get('ver',0))
+        json_result={'error':0}
+        if ver==0:
+            res=weibo_bot.FindReplyForSentence(word_dict_root,req)
+        elif ver==1:
+            res=FindResponse.FindResponse(word_dict_root,req)
+        if len(res)>0:
+            json_result['res']=res
+        else:
+            json_result['error']=1
+            json_result['res']=['不懂你说什么阿']
 
         web.header('Content-Type', 'text/html; charset=utf-8')
-        return json.dumps({'error':0,'res':res},ensure_ascii=False)
+        return json.dumps(json_result,ensure_ascii=False)
 
 if __name__ == '__main__' :
     webapp=web.application(urls, globals())
