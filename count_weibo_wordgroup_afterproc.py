70d0f8ced6b49d9db4ec81f6cea031999cdacedb
amen <amen@localhost.localdomain>
2013-03-14 18:10:45 +0800

    Signed-off-by: amen <amen@localhost.localdomain>

---
+++ count_weibo_wordgroup_afterproc.py
@@ -0,0 +1,26 @@
+#-*-coding:utf-8-*-
+import codecs
+import re
+if __name__ == '__main__':
+    checkwords=[]
+    with codecs.open('data/groupcount.txt','r','utf8') as groupf:
+        for line in groupf:
+            rm=re.match('(?P<word>[^\s]*)\s+(?P<count>\d*)',line)
+            if rm:
+                word=rm.group('word')
+                count=int(rm.group('count'))
+                checkwords.append((word,count))
+    allcount=0
+    for one in checkwords:
+        allcount+=one[1]
+    addcount=0
+    for index in xrange(len(checkwords)):
+        addcount+=checkwords[index][1]
+        if float(addcount)/allcount>0.72:
+            print checkwords[index][0],index
+            break
+    checkwords=checkwords[index+1:]
+
+    with codecs.open('data/groupcount_proced.txt','w','utf8') as groupf:
+        for one in checkwords:
+            print >>groupf,one[0]
--- test_user_tag.py
+++ test_user_tag.py
@@ -4,32 +4,55 @@ import codecs
 import re
 import weibo_bot
 import decoder
+import multiprocessing
+
+word_dict_root=None
+grouptree=None
+def proc_init():
+    global word_dict_root
+    global grouptree
+
+    grouptree=decoder.GroupFinder()
+    grouptree.BuildTree()
+    word_dict_root=decoder.LoadDefaultWordDic()
+def lineproc(id,text):
+    global word_dict_root
+    global grouptree
+
+    grouptree.StartCountGroup()
+    line=weibo_bot.RemoveWeiboRubbish(text)
+    if len(line)==0:
+        return None
+    spliter=decoder.LineSpliter(word_dict_root)
+    spliter.SplitLine(line)
+    spliter.AfterProcess()
+    words=spliter.found_word
+    grouptree.ProcessOneLine(words)
+    return grouptree.group_count
+
 if __name__ == '__main__':
     """
     测试用户的所有微薄，猜测用户的兴趣
     """
     checkwords=[]
-    with codecs.open('data/groupcount.txt','r','utf8') as groupf:
+    with codecs.open('data/groupcount_proced.txt','r','utf8') as groupf:
         for line in groupf:
-            rm=re.match('(?P<word>[^\s]*)\s+(?P<count>\d*)',line)
-            if rm:
-                word=rm.group('word')
-                count=int(rm.group('count'))
-                if count<400:
-                    break
-                elif count<15000:
-                    checkwords.append(word)
+            line=line.strip()
+            checkwords.append(line)
 
     client=weibo_tools.DefaultWeiboClient()
+    wordgroup_allcount={}
+    def lineres(res):
+        if res is None:
+            return
+        for one in res:
+            wordgroup_allcount[one]=wordgroup_allcount.get(one,0)+res[one]
 
-    grouptree=decoder.GroupFinder()
-    grouptree.BuildTree()
-    word_dict_root=decoder.LoadDefaultWordDic()
-
-    #####################init above########################
+    pool=multiprocessing.Pool(initializer=proc_init)
     textlist={}
-    for page in xrange(1,5):
-        res=client.statuses__user_timeline(uid=1766737945,count=100,page=page)
+    proced_ids=set()
+    for page in xrange(1,20):
+        res=client.statuses__user_timeline(uid=1824785034,count=100,page=page)
         print 'page %d read'%page
         if res:
             status=res.get('statuses')
@@ -39,28 +62,22 @@ if __name__ == '__main__':
                 for line in status:
                     id=line['id']
                     text=line['text']
-                    textlist[id]=text
+                    if id not in proced_ids:
+                        proced_ids.add(id)
+                        pool.apply_async(lineproc,(id,text),callback=lineres)
                     retw=line.get('retweeted_status')
                     if retw:
                         id=retw['id']
                         text=line['text']
-                        textlist[id]=text
+                        if id not in proced_ids:
+                            proced_ids.add(id)
+                            pool.apply_async(lineproc,(id,text),callback=lineres)
+    pool.close()
+    pool.join()
 
-    grouptree.StartCountGroup()
-    for line in textlist.values():
-        line=weibo_bot.RemoveWeiboRubbish(line)
-        if len(line)==0:
-            continue
-        spliter=decoder.LineSpliter(word_dict_root)
-        spliter.SplitLine(line)
-        spliter.AfterProcess()
-        words=spliter.found_word
-        grouptree.ProcessOneLine(words)
-
-    itemlist=grouptree.group_count
     groupread=[]
     for wc in checkwords:
-        count=itemlist.get(wc)
+        count=wordgroup_allcount.get(wc)
         if count is not None:
             groupread.append((wc,count))
     groupread.sort(lambda x,y:-cmp(x[1],y[1]))
