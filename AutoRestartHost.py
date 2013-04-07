ceb1c4cf2975e32cc605a284c5345360e716a8c9
amen <amen@localhost.localdomain>
2013-04-07 17:44:56 +0800

    Signed-off-by: amen <amen@localhost.localdomain>

--- AutoRestartHost.py
+++ AutoRestartHost.py
@@ -5,8 +5,15 @@ import time
 if __name__ == '__main__':
     params=['python']
     params[1:]=sys.argv[1:]
-    while True:
-        proc=subprocess.Popen(params)
-        proc.wait()
-        time.sleep(5)
-    pass
+    proc=None
+    try:
+        while True:
+            proc=subprocess.Popen(params)
+            proc.wait()
+            time.sleep(5)
+    except BaseException,e:
+        print e
+        pass
+    finally:
+        if proc and proc.returncode is None:
+            proc.terminate();
